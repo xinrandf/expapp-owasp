3650d8f96a42fe32ae9d869639d4e8c57301a98e
xinrandf <xinrandee@gmail.com>
2025-06-18 14:48:31 -0700

    GAP Week 7 Day 2 Homework

--- app.js
+++ app.js
@@ -7,14 +7,22 @@ const express               =  require('express'),
       LocalStrategy         =  require("passport-local"),
       passportLocalMongoose =  require("passport-local-mongoose"),
       User                  =  require("./models/user")
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 
 //Connecting database
 mongoose.connect("mongodb://localhost/auth_demo");
-
 app.use(expSession({
     secret:"mysecret",       //decode or encode session
     resave: false,          
-    saveUninitialized:false
+    saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    secure: true,
+    maxAge: 1 * 60 * 1000 // 10 minutes
+  }
 }))
 
 passport.serializeUser(User.serializeUser());       //session encoding
@@ -32,12 +40,27 @@ app.use(express.static("public"));
 //=======================
 //      O W A S P
 //=======================
+const xss = require('xss-clean');
+//Data Sanitization against NoSQL Injection Attacks
+app.use(mongoSanitize());
 
+// Preventing Brute Force & DOS Attacks – Rate Limiting
+const limit = rateLimit({
+  max: 100, // max requests
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  message: 'Too many requests' // message to send
+});
+app.use('/login', limit); // Setting limiter on /login route
+
+// Preventing DOS Attacks – Body Parser
+app.use(express.json({ limit: '10kb' })); // Body limit is 10kb
 
 
 //=======================
 //      R O U T E S
 //=======================
+app,use(helmet()); 
+
 app.get("/", (req,res) =>{
     res.render("home");
 })
